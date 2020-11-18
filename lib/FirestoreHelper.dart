4d589d3fe9d99f1d85c7d0ea4a5cdf8befa3673b
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2020-11-18 18:50:56 +0000

    add : setData

--- lib/FirestoreHelper.dart
+++ lib/FirestoreHelper.dart
@@ -1,1 +1,7 @@
-class FirestoreHelper {}
+class FirestoreHelper {
+  final _firestore;
+
+  Future<void> setData({String path, Map<String, dynamic> data}) async {
+    await _firestore.doc(path).set(data, SetOptions(merge: true));
+  }
+}
