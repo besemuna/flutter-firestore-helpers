cf9b9d479f752dd82402b8320965774316538ecb
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2020-11-19 23:14:40 +0000

    add : addDocument

--- lib/FirestoreHelper.dart
+++ lib/FirestoreHelper.dart
@@ -4,4 +4,12 @@ class FirestoreHelper {
   Future<void> setData({String path, Map<String, dynamic> data}) async {
     await _firestore.doc(path).set(data, SetOptions(merge: true));
   }
+
+  Future<DocumentReference> addDocument({
+    String path,
+    Map<String, dynamic> data,
+  }) async {
+    final reference = _firestore.collection(path);
+    return reference.add(data);
+  }
 }
