30859d7b9bef52f175a3a58c4594e2a643446e23
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2020-11-20 18:26:31 +0000

    add : setup firebase

--- lib/FirestoreHelper.dart
+++ lib/FirestoreHelper.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class FirestoreHelper {
-  final _firestore;
+  final _firestore = FirebaseFirestore.instance;
 
   Future<void> setData({String path, Map<String, dynamic> data}) async {
     await _firestore.doc(path).set(data, SetOptions(merge: true));
